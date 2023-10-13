c640303bbf144e3b1d14a4f59ac400e9b21a7650
hornsp <144290556+hornsp@users.noreply.github.com>
2023-10-12 21:17:41 -0400

    Main layout completed

---
+++ penny-project/src/App.vue
@@ -0,0 +1,63 @@
+
+<template>
+  <body>
+  <div class="p-d-flex p-jc-center p-ai-center">
+    <CardCustom title="PrimeVue Card Example" style="width: 300px">
+      <template #content>
+        <p>{{ walkStatus }}</p>
+        <ButtonCustom @click="changePennyWalkStatus" label="Walker Counter" icon="pi pi-plus"/>
+      </template>
+    </CardCustom>
+    <CardCustom title="PrimeVue Card Example" style="width: 300px">
+      <template #content>
+        <p>Has Penny had Breakfast?</p>
+        <SelectButtonCustom v-model="breakfastValue" :options="breakfastOptions" aria-labelledby="basic" />
+      </template>
+    </CardCustom>
+    <CardCustom title="PrimeVue Card Example" style="width: 300px">
+      <template #content>
+        <p>Has Penny had Dinner?</p>
+        <SelectButtonCustom v-model="dinnerValue" :options="dinnerOptions" aria-labelledby="basic" />
+      </template>
+    </CardCustom>
+  </div>
+</body>
+</template>
+
+<script>
+export default {
+  setup() {
+    const handleButtonClick = () => {
+      console.log('Button clicked!');
+    };
+    return {
+      handleButtonClick,
+    };
+  },
+  data(){
+    return {
+      walkCounter: 0,
+      walkStatus: 'Has penny been walked today?',
+      breakfastValue: 'No',
+      dinnerValue: 'No',
+      breakfastOptions: ['No', 'Yes'],
+      dinnerOptions: ['No', 'Yes'],
+    };
+  },
+  methods: {
+    changePennyWalkStatus() {
+      this.walkCounter += 1;
+      this.walkStatus = `Penny has been walked ${this.walkCounter} time${this.walkCounter === 1 ? '.' : 's.'}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+body{
+  background-image: url('./assets/PennyPicture.jpeg');
+  background: cover;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
---
+++ penny-project/src/main.js
@@ -0,0 +1,25 @@
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+// Import PrimeVue components and styles
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
+
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import SelectButton from 'primevue/selectbutton';
+
+// Create the Vue app instance and register PrimeVue components
+const app = createApp(App);
+
+app.component('CardCustom', Card);
+app.component('ButtonCustom', Button);
+app.component('InputTextCustom', InputText)
+app.component('SelectButtonCustom', SelectButton)
+
+
+// Mount the app
+app.mount('#app');
